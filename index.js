e27fce9729cfbfadabc4a41876d7ca6e204d53c2
Marcus <marcus.daley1@outlook.com>
2021-04-02 15:26:24 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+    return drivers.push('Ralph')
+}
+function destructivelyPrependDriver(name) {
+    return drivers.unshift('Bob')
+}
+function destructivelyRemoveLastDriver() {
+    return drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift()
+}
+
+function appendDriver (name) {
+    return [...drivers, 'Broom'];n
+  }
+  
+  function prependDriver (name) {
+    return ['Arnold', ...drivers];
+  }
+  
+  function removeFirstDriver () {
+    return drivers.slice(1);
+  }
+  
+  function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+  }
